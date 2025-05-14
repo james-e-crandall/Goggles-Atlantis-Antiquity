171e3542041734807bab3aa1052b058a2232e06f
james-e-crandall <james.e.crandall@gmail.com>
2025-05-14 12:32:03 -0500

    visual remap, wonder, text

---
+++ modules/ui/game.js
@@ -0,0 +1,17 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_WAR_CHARIOT': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UUNIT_WAR_CHARIOT_2': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }
+}
+
+
+
+
+
+
