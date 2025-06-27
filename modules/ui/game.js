b73fdba231a34c8ce9ca17a1a39d1beb3868c76d
james-e-crandall <james.e.crandall@gmail.com>
2025-06-27 08:45:14 -0500

    JEC

--- modules/ui/game.js
+++ modules/ui/game.js
@@ -1,10 +1,10 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
-    'UNIT_WAR_CHARIOT': { //your unit type
+    'UNIT_WAR_CHARIOT_JEC': { //your unit type
         unitType: 'UNIT_CHARIOT',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UUNIT_WAR_CHARIOT_2': { //your unit type
+    'UUNIT_WAR_CHARIOT_JEC_2': { //your unit type
         unitType: 'UNIT_CHARIOT',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     }
